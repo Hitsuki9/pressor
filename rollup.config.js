01b4a6ff3d29d3b41f1c82e4f788ea8f591faa33
wangxin <wangxin@thothinfo.com>
2020-05-26 09:07:35 +0800

    fix: fixed @rollup/plugin-babel import bug

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import babel from '@rollup/plugin-babel';
+import { babel } from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import { name, expose } from './package.json';
